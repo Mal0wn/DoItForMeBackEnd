9f98cb687209a08960e30400841d61dfd9a6e9a4
Marine M <marine.michelot-cic@ibm.com>
2022-10-20 13:51:29 +0200

    create connection ok

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import mysql from 'mysql2';
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const db = mysql.createConnection({
+  host: process.env.MY_SQL_DB_HOST,
+  user: process.env.MY_SQL_DB_USER,
+  password: process.env.MY_SQL_DB_PASSWORD,
+  database: process.env.MY_SQL_DB_DATABASE,
+  
+});
+
+export default db;
 
 const app: Application = express();
 
@@ -7,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
+
   res.send('Hello Mother Fuckers ! ')
 })
 
