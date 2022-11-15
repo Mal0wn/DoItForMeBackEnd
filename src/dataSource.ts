a5e698618b98c1bd5f2c774a12ef177026701b26
Marine M <marine.michelot-cic@ibm.com>
2022-11-15 14:54:44 +0100

    change info for datasource

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: "./.env" });
 
 export const dataSource = new DataSource({
     type: "mysql",
-    host: process.env.HOST ?? "localhost",
+    host: process.env.DB_HOST ?? "localhost",
     port: parseInt(process.env.DB_PORT ?? "3306"),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
