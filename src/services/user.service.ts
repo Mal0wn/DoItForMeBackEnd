2ae6c8b455cd107300237ac3435bdf6177bc5f73
Nicolas De waegenaere <de_waegenaere@live.fr>
2023-05-02 14:50:02 +0200

    bugfix(service): delete condition if address is null

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -172,29 +172,15 @@ const userService = {
             dbUser.birthday = user.birthday;
             dbUser.phone = user.phone;
             
-            // If user has no address, create one and save it else update it
-            if (dbUser.address.length === 0) {
-                const address = new Address();
-                address.number = user.address[0].number;
-                address.street = user.address[0].street;
-                address.zip_code = user.address[0].zip_code;
-                address.city = user.address[0].city;
-                address.country = user.address[0].country;
-                address.complement = user.address[0].complement;
-                address.id_user = user.id;
-                dbUser.address = [address];
+            // Update address                
+            dbUser.address[0].number = user.address[0].number;
+            dbUser.address[0].street = user.address[0].street;
+            dbUser.address[0].zip_code = user.address[0].zip_code;
+            dbUser.address[0].city = user.address[0].city;
+            dbUser.address[0].country = user.address[0].country;
+            dbUser.address[0].complement = user.address[0].complement;
 
-                AddressRepository.save(dbUser.address);
-            } else {                
-                dbUser.address[0].number = user.address[0].number;
-                dbUser.address[0].street = user.address[0].street;
-                dbUser.address[0].zip_code = user.address[0].zip_code;
-                dbUser.address[0].city = user.address[0].city;
-                dbUser.address[0].country = user.address[0].country;
-                dbUser.address[0].complement = user.address[0].complement;
-
-                AddressRepository.save(dbUser.address[0]);
-            }
+            AddressRepository.save(dbUser.address[0]);
             
             // Save user in database
             return UserRepository.save(dbUser);
